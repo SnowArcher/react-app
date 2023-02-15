88ce13307f29602492f01741802bab934d4fc1a0
Oksana Synianska <sinyanskayaoksana@gmail.com>
2023-02-15 15:15:34 +0200

    add info to team

--- src/components/about-fund/team-partners/OurTeam.js
+++ src/components/about-fund/team-partners/OurTeam.js
@@ -2,53 +2,43 @@ function OurTeam () {
     return (
         <>
         <div className="team">
-            <h2>Our team</h2>
+            <h2>Наша команда</h2>
             <div className="team__card">
-                <img src="../img/Image2.png" alt="photo_item"/>
+                <img src="../img/img_team/Grybkov.png" alt="photo_item"/>
                 <div className="team__card_description">
-                    <h4>Name</h4>
-                    <h5>Work field</h5>
-                    <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo suscipiantur mel.Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo suscipiantur mel.</p>
+                    <h4>Роман Грибков</h4>
+                    <h5>Голова фонду</h5>
+                    <p>Засновник та людина, котра зібрала та надихнула усю нашу команду. До війни Роман мав успішний бізнес у сфері дитячої освіти. Але з перших днів війни він разом зі своїми партнерами організував на базі дитячого центру «Арбуз» - хаб гуманітарної допомоги, Благодійний фонд «A-HELP UKRAINE».</p>
                 </div>
             </div>
             <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo suscipiantur mel:</p>
         </div>    
         <ul className="team-items">
             <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
+                <img src="../img/img_team/Pushai.png" alt="photo_item"/>
+                <h4>Пушай Ірина Анатоліївна</h4>
+                <p>Cпівзасновник, менеджер проектів</p>
+
             </li>
             <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
+                <img src="../img/img_team/Vodomerova.png" alt="photo_item"/>
+                <h4>Водомєрова Ірина Володимирівна</h4>
+                <p>Керівник фонду</p>
             </li>
             <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
+                <img src="../img/img_team/Shamaiko.png" alt="photo_item"/>
+                <h4>Шамайко Наталія Олександрівна</h4>
+                <p>Вихователь дошкільної освіти та організатор музичної діяльності</p>
             </li>
             <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
+                <img src="../img/img_team/Dolgov.png" alt="photo_item"/>
+                <h4>Долгов Сергій Єлізарович</h4>
+                <p>Водій-волонтер</p>
             </li>
             <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
-            </li>
-            <li>
-                <img src="../img/Image5.png" alt="photo_item"/>
-                <h4>Name Verylonglastname</h4>
-                <p>Work</p>
-                <p>field</p>
+                <img src="../img/img_team/Popova.png" alt="photo_item"/>
+                <h4>Попова Юлія Олександрівна</h4>
+                <p>Волонтер</p>
             </li>
         </ul>
         <div className="partners">
@@ -61,14 +51,17 @@ function OurTeam () {
             <li>
                 <img src="../img/Image6.png" alt="photo_item"/>
                 <h4>Name</h4>
+
             </li>
             <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
+                <img src="../img/img_team/avsi.png" alt="photo_item"/>
+                <h4>AVSI</h4>
+                <p>Міжнародна організація</p>
             </li>
             <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
+                <img src="../img/img_team/sila.png" alt="photo_item"/>
                 <h4>Name</h4>
+                <p>Громадська організація</p>
             </li>
             <li>
                 <img src="../img/Image6.png" alt="photo_item"/>
