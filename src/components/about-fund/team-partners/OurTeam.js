f46e3c2cfb41479162937f588fbab7569e8ddc63
Oksana Synianska <sinyanskayaoksana@gmail.com>
2023-02-16 18:01:43 +0200

    add info to team

--- src/components/about-fund/team-partners/OurTeam.js
+++ src/components/about-fund/team-partners/OurTeam.js
@@ -3,83 +3,112 @@ function OurTeam () {
         <>
         <div className="team">
             <h2>Наша команда</h2>
-            <div className="team__card">
                 <img src="../img/img_team/Grybkov.png" alt="photo_item"/>
-                <div className="team__card_description">
-                    <h4>Роман Грибков</h4>
-                    <h5>Голова фонду</h5>
-                    <p>Засновник та людина, котра зібрала та надихнула усю нашу команду. До війни Роман мав успішний бізнес у сфері дитячої освіти. Але з перших днів війни він разом зі своїми партнерами організував на базі дитячого центру «Арбуз» - хаб гуманітарної допомоги, Благодійний фонд «A-HELP UKRAINE».</p>
-                </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo suscipiantur mel:</p>
+                <h4>Роман Грибков</h4>
+                <h5>Засновник фонду</h5>
+                <p>Засновник та людина, котра зібрала та надихнула усю нашу команду. До війни Роман мав успішний бізнес у сфері дитячої освіти. Але з перших днів війни він разом зі своїми партнерами організував на базі дитячого центру «Арбуз» - хаб гуманітарної допомоги, Благодійний фонд «A-HELP UKRAINE».</p>
         </div>    
         <ul className="team-items">
             <li>
+                <img src="../img/img_team/Vodomerova.png" alt="photo_item"/>
+                <h4>Водомєрова</h4>
+                <h4>Ірина</h4>
+                <h4>Володимирівна</h4>
+                <p>Cпівзасновник, керівник фонду</p>
+            </li>
+            <li>
                 <img src="../img/img_team/Pushai.png" alt="photo_item"/>
-                <h4>Пушай Ірина Анатоліївна</h4>
+                <h4>Пушай</h4>
+                <h4>Ірина</h4>
+                <h4>Анатоліївна</h4>
                 <p>Cпівзасновник, менеджер проектів</p>
 
             </li>
             <li>
-                <img src="../img/img_team/Vodomerova.png" alt="photo_item"/>
-                <h4>Водомєрова Ірина Володимирівна</h4>
-                <p>Керівник фонду</p>
-            </li>
-            <li>
                 <img src="../img/img_team/Shamaiko.png" alt="photo_item"/>
-                <h4>Шамайко Наталія Олександрівна</h4>
-                <p>Вихователь дошкільної освіти та організатор музичної діяльності</p>
-            </li>
-            <li>
-                <img src="../img/img_team/Dolgov.png" alt="photo_item"/>
-                <h4>Долгов Сергій Єлізарович</h4>
-                <p>Водій-волонтер</p>
+                <h4>Шамайко</h4>
+                <h4>Наталія</h4>
+                <h4>Олександрівна</h4>
+                <p>Волонтер</p>
             </li>
             <li>
                 <img src="../img/img_team/Popova.png" alt="photo_item"/>
-                <h4>Попова Юлія Олександрівна</h4>
+                <h4>Попова</h4>
+                <h4>Юлія</h4>
+                <h4>Олександрівна</h4>
                 <p>Волонтер</p>
+            </li>
+            <li>
+                <img src="../img/img_team/Dolgov.png" alt="photo_item"/>
+                <h4>Долгов</h4>
+                <h4>Сергій</h4>
+                <h4>Єлізарович</h4>
+                <p>Водій-волонтер</p>
             </li>
         </ul>
         <div className="partners">
             <h2>
-                Our partners
+                Наші партнери
             </h2>
-            <p>Lorem ipsum dolor sit amet, cum sapientem honestatis ea, verear labores feugait sea in, cu justo suscipiantur mel.</p>
+            <ul className="partners_items">
+                <li>
+                    <div>
+                        <img src="../img/img_team/WFP.png" alt="photo_item"/>
+                    </div>
+                    <h4>World Food Programme</h4>
+                    <p>Гумінатарна організація</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/Golosy.png" alt="photo_item"/>
+                    </div>
+                    <h4>Голоси дітей</h4>
+                    <p>Благодійний фонд</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/sila.png" alt="photo_item"/>
+                    </div>
+                    <h4>Сила берегині</h4>
+                    <p>Громадська організація</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/WCK.png" alt="photo_item"/>
+                    </div>    
+                    <h4>World Central Kitchen</h4>
+                    <p>Благодійна організація</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/Infochat.png" alt="photo_item"/>
+                    </div>    
+                    <h4>Інфочат Харків</h4>
+                    <p>Громадська організація</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/goodbread.png" alt="photo_item"/>
+                    </div>
+                    <h4>Good bread</h4>
+                    <p>Соціальна пекарня</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/yaposhka.png" alt="photo_item"/>
+                    </div>
+                    <h4>Япошка</h4>
+                    <p>Мережа ресторанів японської та італійської кухні</p>
+                </li>
+                <li>
+                    <div>
+                        <img src="../img/img_team/global2000.png" alt="photo_item"/>
+                    </div>    
+                    <h4>GLOBAL 2000</h4>
+                    <p>Організація з охорони навколишнього середовища</p>
+                </li>
+            </ul>
         </div>
-        <ul className="partners-items">
-            <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
-
-            </li>
-            <li>
-                <img src="../img/img_team/avsi.png" alt="photo_item"/>
-                <h4>AVSI</h4>
-                <p>Міжнародна організація</p>
-            </li>
-            <li>
-                <img src="../img/img_team/sila.png" alt="photo_item"/>
-                <h4>Name</h4>
-                <p>Громадська організація</p>
-            </li>
-            <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
-            </li>
-            <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
-            </li>
-            <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
-            </li>
-            <li>
-                <img src="../img/Image6.png" alt="photo_item"/>
-                <h4>Name</h4>
-            </li>
-        </ul>
         </>
     );
 }
